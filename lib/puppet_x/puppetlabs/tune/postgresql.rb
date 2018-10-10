db3dd54669f9a3c6dfbe7c9c5427bebd5c79a3d8
tkishel <tkishel@gmail.com>
2018-10-10 14:10:20 -0700

    wip

--- lib/puppet_x/puppetlabs/tune/postgresql.rb
+++ lib/puppet_x/puppetlabs/tune/postgresql.rb
@@ -1,22 +1,50 @@
+def other_database_tunable_param_names
+  [
+    'puppet_enterprise::profile::database::autovacuum_max_workers'
+    'puppet_enterprise::profile::database::autovacuum_work_mem'
+    'puppet_enterprise::profile::database::effective_cache_size'
+    'puppet_enterprise::profile::database::log_temp_files'
+    'puppet_enterprise::profile::database::maintenance_work_mem'
+    'puppet_enterprise::profile::database::work_mem'
+  ]
+end
+
 def calculate_database_settings(node)
-  percent_ram_database = 25
-  minimum_ram_database = fit_to_memory(node['resources']['ram'], 2048, 3072, 4096)
-  maximum_ram_database = 16384
+  percent_ram_shared_buffers         = 25
+  minimum_ram_shared_buffers         = fit_to_memory(node['resources']['ram'], 2048, 3072, 4096)
+  maximum_ram_shared_buffers         = 16384
+  percent_cpu_autovacuum_max_workers = 33.3
+  minimum_cpu_autovacuum_max_workers = 3
+  maximum_cpu_autovacuum_max_workers = 8
+  minimum_ram_effective_cache_size   = 128
+  maximum_ram_maintenance_work_mem   = 1024
+  minimum_ram_work_mem               = 4
+  maximum_ram_work_mem               = 16
+
+  minimum_ram_os = (@options[:memory_reserved_for_os] != 0) ? @options[:memory_reserved_for_os] : @defaults[:memory_reserved_for_os]
 
   settings = initialize_host_settings(node)
 
-  ram_database = calculate_ram(node, database_settings['totals']['RAM']['used'], percent_ram_database, minimum_ram_database, maximum_ram_database)
-  return {} unless ram_database
-  settings['params']['puppet_enterprise::profile::database::shared_buffers'] = "#{ram_database}MB"
-  settings['totals']['RAM']['used'] += ram_database
+  ram_shared_buffers = calculate_ram(node, settings['totals']['RAM']['used'], percent_ram_shared_buffers, minimum_ram_shared_buffers, maximum_ram_shared_buffers)
+  return {} unless ram_shared_buffers
+  settings['params']['puppet_enterprise::profile::database::shared_buffers'] = "#{ram_shared_buffers}MB"
+  settings['totals']['RAM']['used'] += ram_shared_buffers
 
-  available_gb_for_postgres  = (settings['totals']['RAM']['used'] / 1024)
-  cpu_autovacuum_max_workers = [3, [8, (node['resources']['cpu'] / 3).to_i].min].max
-  ram_effective_cache_size   = [((available_ram_for_postgres * 3) / 5).to_i, 128].max
-  ram_maintenance_work_mem   = (available_ram_for_postgres / 8).to_i
+  # Subtract settings['totals']['RAM']['used'] from node['resources']['ram'] ?
+  available_ram_mb = node['resources']['ram'] - minimum_ram_os
+  available_ram_gb = (available_mb_for_cache_size / 1024.0).to_i
+
+  # These are equivalent to the defaults in puppetlabs-puppet_enterprise/manifests/params.pp and puppetlabs-puppet_enterprise/manifests/profile/database.pp
+  ram_work_mem               = clamp(minimum_ram_work_mem, ((available_ram_gb / 8.0) + 0.5).to_i, maximum_ram_work_mem)
+  cpu_autovacuum_max_workers = calculate_cpu(node['resources']['cpu'], 0, percent_cpu_autovacuum_max_workers, minimum_cpu_autovacuum_max_workers, maximum_cpu_autovacuum_max_workers)
+  ram_maintenance_work_mem   = [maximum_ram_maintenance_work_mem, (node['resources']['ram'] / 8.0).to_i].min
   ram_autovacuum_work_mem    = (ram_maintenance_work_mem / cpu_autovacuum_max_workers).to_i
-  ram_work_mem               = clamp(4, ((available_gb_for_postgres / 8.0) + 0.5).to_i, 16)
-  log_temp_files             = (ram_work_mem * 1024)
+  ram_effective_cache_size   = [((available_ram_mb * 3) / 5.0).to_i, minimum_ram_effective_cache_size].max
+  log_temp_files             = (ram_work_mem * 1024) # specified in kilobytes # log_temp_files should equal work_mem to avoid logging temp files smaller than work_mem
+
+  # shared_buffers: system memory < 2GB set to 20% of total system memory; < 64GB set to 25%; > 64GB set to 16GB
+  # maintenance_work_mem: 10% of system memory, up to 1GB
+  # effective_cache_size: 75% of system memory
 
   settings['params']['puppet_enterprise::profile::database::autovacuum_max_workers'] = cpu_autovacuum_max_workers
   settings['params']['puppet_enterprise::profile::database::autovacuum_work_mem']    = "#{ram_autovacuum_work_mem}MB"
@@ -24,9 +52,6 @@ def calculate_database_settings(node)
   settings['params']['puppet_enterprise::profile::database::log_temp_files']         = log_temp_files
   settings['params']['puppet_enterprise::profile::database::maintenance_work_mem']   = "#{ram_maintenance_work_mem}MB"
   settings['params']['puppet_enterprise::profile::database::work_mem']               = "#{ram_work_mem}MB"
-
-  # Do not add the following, as they are not allocations: cpu_autovacuum_max_workers, log_temp_files, ram_effective_cache_size
-  settings['totals']['RAM']['used'] += ram_buffers + ram_work_mem + ram_maintenance_work_mem + (ram_autovacuum_work_mem * cpu_autovacuum_max_workers)
 
   settings
 end
